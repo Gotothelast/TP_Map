053dd5ffa3ffa5f997b850a871f33ee709b74c41
hugo lafe <hugolafe@MacBook-Pro-de-hugo.local>
2022-11-10 13:20:06 +0100

    site et favoris fonctionnel

--- JSMap.js
+++ JSMap.js
@@ -7,25 +7,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var iconimgurl = 'image/GreenMarker.png';
-var RedIcon = L.icon
-({
-    iconUrl: 'image/RedMarker.png',
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-})
 
-var GreenIcon = L.icon
-({
-    iconUrl: 'image/GreenMarker.png',
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-})
 //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 var api= "https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=e5cf20fe05e76267eaf0161b758ec0ea1179d0cb";
 
@@ -42,8 +24,7 @@ function loadDoc(url,cfunction) {
 
 function addFav(i)
 {
-    localStorage.setItem('Fav' + i, i);
-    console.log(localStorage.getItem('Fav' + i));
+    localStorage.setItem('Fav'+i, i);
 
 }
 
@@ -52,16 +33,13 @@ function bike(xhttp) {
     console.log(obj);
     for (i=0 ; i<obj.length ; i++)
     {
-if (localStorage.getItem('Fav' + i)===true)
-{
-    //Comparé la station actuelle dans le tableau avec le contenu du local storage
-    // if Actuelle station in local storage :
-    iconimgurl='image/etoile.png'
-
-}
     if (obj[i].available_bikes===0)
     {
         iconimgurl = 'image/RedMarker.png';
+    }
+    else if (localStorage.getItem('Fav'+i))
+    {
+        iconimgurl = 'image/etoile.png';
     }
     else
     {
@@ -70,20 +48,18 @@ if (localStorage.getItem('Fav' + i)===true)
         var icon = L.icon
         ({
             iconUrl: iconimgurl,
-            iconSize:     [38, 95], // size of the icon
-            shadowSize:   [50, 64], // size of the shadow
+            iconSize:     [20, 38], // size of the icon
             iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-            shadowAnchor: [4, 62],  // the same for the shadow
             popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
         })
 
-
-            marker = L.marker([obj[i].position.lat, obj[i].position.lng],{icon:icon}).addTo(map).bindPopup("  Adresse:"+obj[i].address+".  Vélo dispo:"+obj[i].available_bikes+ "<button onclick=addFav("+ i +"),loadDoc(api,bike)>Print</button>");
-
+        marker = L.marker([obj[i].position.lat, obj[i].position.lng],{icon:icon}).addTo(map).bindPopup("  Adresse:"+obj[i].address+".  Vélo dispo:"+obj[i].available_bikes+ "<button onclick=addFav("+ i +"),loadDoc(api,bike)>Print</button>");
 
 
 
     }
+
+
 }
 
 
